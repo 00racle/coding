7ada9668f15b945945373dbedd51a67087db5b9b
0racle082 <haha082@hanmail.net>
2019-03-18 18:45:34 +0900

    2019.03.18 commit

--- python/kca/db_query/test.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-f = open('/root/crawler/test/test/db_query/test.txt', 'a')
-f.write("현재시각: "+str(datetime.now())+"\n")
-f.close()
--- python/kca/db_query/test1.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.call(['python3', '/root/crawler/test/test/db_query/test.py'])
