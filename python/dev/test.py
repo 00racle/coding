577b642234dec9fda8c71fd904a9e8ba3f6dc119
0racle082 <haha082@hanmail.net>
2019-03-22 17:59:25 +0900

    2019.03.22 commit

--- python/dev/test.py
+++ python/dev/test.py
@@ -1,7 +1,12 @@
-class Time:
-	def __init__(self, hour, minute, second):
-		self.hour = hour
-		self.minute = minute
-		self.second = second
+import math
 
-	def is_time_valid(
+class Point2D:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+length = 0.0
+
+p = [Point2D(), Point2D(), Point2D(), Point2D()]
+
+p[0].x, p[0].y, p[1].x, p[1].y, p[2].x, p[2].y, p[3].x, p[3].y = map(int, input().split())
