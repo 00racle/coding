0ec90cfd4e2265d1ff5068b8b96936f1e0b2bef9
0racle082 <haha082@hanmail.net>
2019-03-26 16:21:01 +0900

    2019.03.26 commit

---
+++ python/dev/zipfunc.py
@@ -0,0 +1,37 @@
+import zipfile
+import threading
+import time
+
+start_time = time.time()
+
+fin = True
+
+pwlist = []
+for i in range(100000):
+	pwlist.append(str(i))
+
+def Extract(zFile, pwstr):
+	try:
+		global fin
+		zFile.extractall(pwd = pwstr.encode())
+		print("Found Password : {0} ".format(pwstr))
+		fin = False
+	except:
+		#print("Wrong Password: {0} ".format(pwstr))
+		pass
+
+def main():
+	zFile = zipfile.ZipFile('test.zip', 'r')
+
+	print("Extraction Start -------------------- \n")
+	for i in pwlist:
+		Extract(zFile, i)
+		if fin == False:
+			break
+
+if __name__=='__main__':
+	main()
+
+end_time = time.time()
+
+print("Runtime : %0.6f"%(end_time - start_time))
---
+++ python/dev/zipthread.py
@@ -0,0 +1,32 @@
+import zipfile
+import threading
+import time
+
+start_time = time.time()
+
+pwlist = []
+for i in range(100000):
+	pwlist.append(str(i))
+
+def Extract(zFile, pwstr):
+	try:
+		global fin
+		zFile.extractall(pwd = pwstr.encode())
+		print("Found Password : {0} ".format(pwstr))
+	except:
+		#print("Wrong Password : {0} ".format(pwstr))
+		pass
+
+def main():
+	zFile = zipfile.ZipFile('test.zip', 'r')
+
+	print("Extraction Start -------------------- \n")
+	for i in pwlist:
+		t = threading.Thread(target=Extract, args=(zFile, i))
+		t.start()
+
+if __name__ == '__main__':
+	main()
+
+end_time = time.time()
+print("Runtime : %0.6f"%(end_time - start_time))
