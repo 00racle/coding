92cbc0d4516118c35b4ec1902ccb29dc3a985977
0racle082 <haha082@hanmail.net>
2019-04-07 19:22:57 +0900

    2019.04.07 commit

---
+++ python/programmers/same_number.py
@@ -0,0 +1,17 @@
+arr = [1,1,3,3,0,1,1]
+
+'''
+def solution(arr):
+    answer = []
+    temp = 0
+    for i in arr:
+        if i != temp:
+            temp = i
+            answer.append(i)
+    return answer
+'''
+
+def solution(arr):
+    return 
+
+print(solution(arr))
