911474ec6f62c2983e5579ef954c6db8140d8e52
0racle082 <haha082@hanmail.net>
2019-04-26 18:01:41 +0900

    2019.04.26 commit

--- python/dev/test.py
+++ python/dev/test.py
@@ -1,1 +1,11 @@
-print('{0:03d}'.format(114))
+def palindrome(word):
+	l = list(word)
+	if l != l[::-1]:
+		raise NotPalindromeError('회문이 아닙니다')
+	print(word)
+
+try:
+	word = input("회문 판별 문자열 입력: ")
+	palindrome(word)
+except NotPalindromeError as e:
+	print(e)
