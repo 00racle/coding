c7140df0fb24ca07f239e521623cb8dbcabc91d8
0racle082 <haha082@hanmail.net>
2019-09-02 17:33:15 +0900

    2019.09.02 commit

--- python/programmers/training_clothes.py
+++ python/programmers/training_clothes.py
@@ -1,14 +1,17 @@
 import time
 
 start = time.time()
-n = 5
-lost = [2,4]
-reserve = [1,3,5]
+#n = 3
+#lost = [1,2]
+#reserve = [2,3]
+# 답 2
+n = 24
+lost = [12, 13, 16, 17, 19, 20, 21, 22]
+reserve = [1, 22, 16, 18, 9, 10]
+# 답 19
 
-#lost = [2,4]
-#reserve = [3]
-'''
 def solution(n, lost, reserve):
+	'''
 	cnt = n - len(lost)
 	for i in reserve:
 		if i-1 in lost:
@@ -17,17 +20,9 @@ def solution(n, lost, reserve):
 		elif i+1 in lost:
 			cnt += 1
 			lost.remove(i+1)
-'''
-def solution(n, lost, reserve):
-	cnt = n - len(lost)
-	for i in reserve:
-		if i-1 in lost:
-			cnt += 1
-			lost.pop()
-		elif i+1 in lost:
-			cnt += 1
-			lost.pop()
 	return cnt
+	'''
+
 print(solution(n, lost, reserve))
 
 end = time.time()
