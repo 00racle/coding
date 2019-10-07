6aab89c33aee51bbc3eeb4b32d6e201bd2086e28
0racle082 <haha082@hanmail.net>
2019-10-07 20:18:50 +0900

    2019.10.07 commit

--- python/programmers/strange_string.py
+++ python/programmers/strange_string.py
@@ -1,22 +1,21 @@
-s = "try hello world"
+#s = "try  hello world"
+s = "try hello world strys try"
+#s = "sp  ace"
 
 def solution(s):
-	'''
-	l = list(s)
-	for i in range(len(l)):
-		if i%2 == 0:
-			l[i] = l[i].upper()
-	
-	return ''.join(l)
-	'''
-	'''
-	answer = ''
-	for i in range(len(s)):
-		if i%2 == 0:
-			answer += chr(ord(s[i])-32)
-		else:
-			answer += s[i]
-	'''
-	
-solution(s)
-#print(solution(s))
+    answer = ""
+    l = s.split(" ")
+    for i in l:
+        for j in range(len(i)):
+            if j%2 == 0:
+                answer += i[j].upper()
+            else:
+                answer += i[j]
+        answer += " "
+
+    answer = answer.strip()
+
+    return answer + "."
+
+            
+print(solution(s))
