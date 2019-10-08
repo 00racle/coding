debe74e7e02af45517fe7e3332e0b872d8e25d9a
0racle082 <haha082@hanmail.net>
2019-10-09 01:01:45 +0900

    2019.10.09 commit

--- c/programmers/level1/zegop.c
+++ c/programmers/level1/zegop.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 0, 1, 4, 5, 6, 9
+
 long long solution(long long n)
 {
 	long long answer = 0;
-
-
+	int i;
+	int s;
+	if(n%1 == 0 || n%1 == 1 || n%1 == 4 || n%1 == 5 || n%1 == 6 || n%1 == 9)
+	{
+		for(i=0; i<n/2; i++)
+		{
+			if(i*i == n)
+			{
+				s = i;
+				break;
+			}
+		}
+		answer = (s+1)*(s+1);
+	}
+	else
+	{
+		answer = -1;
+	}
 	return answer;
+}
+
+int main(void)
+{
+	long long n;
+	printf("정수 n입력: ");
+	scanf("%lld", &n);
+	printf("결과 값은: %lld \n", solution(n));
+	return 0;
 }
