40292d29ee020c5150b17ec1ad7ab4735f0ec737
0racle082 <haha082@hanmail.net>
2019-10-08 16:45:18 +0900

    2019.10.08 commit

--- c/test.c
+++ c/test.c
@@ -1,49 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-int Solution(int a, int b)
-{
-	int sum=0;
-	int i;
-	if(a>b)
-	{
-		for(i=b; i<=a; i++)
-			sum += i;
-		return sum;
-	}
-	else
-	{
-		for(i=a; i<=b; i++)
-			sum += i;
-		return sum;
-	}
-}
-
-
 int main(void)
 {
-<<<<<<< HEAD
-	int arr[3][9];
-	int i, j;
-
-	for(i=0; i<3; i++)
-		for(j=0; j<9; j++)
-			arr[i][j] = (i+2) * (j+1);
-
-
-	for(i=0; i<3; i++)
-	{
-		for(j=0; j<9; j++)
-			printf("%4d", arr[i][j]);
-		printf("\n");
-	}
-=======
-	int a, b;
-
-	printf("정수 두개 입력하세요!");
-	scanf("%d %d", &a, &b);
-
-	printf("결과값은 : %d \n", Solution(a, b));
->>>>>>> 9453cf932445a1b88cab0129127be373671c41f3
+	int num = 3;
+	printf("%d \n", num*3+1);
 	return 0;
 }
