9bcd07a4bd54534ddc590db83717059b4c33b101
0racle082 <haha082@hanmail.net>
2019-10-14 17:29:12 +0900

    2019.10.14 commit

---
+++ c/programmers/level2/skilltree.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int solution(char* skill, char* skill_trees[], size_t skill_trees_len)
+{
+	int answer = 0;
+	int i, j;
+	for(i=0; i<skill_trees_len; i++)
+	{
+		printf("%s \n", skill_trees[i]);
+		/*for(j=0; j<sizeof(skill_trees[i]); j++)
+		{
+			printf("%c ", skill_trees[i][j]);
+		}
+		printf("\n");*/
+	}
+
+	return answer;
+}
+
+int main(void)
+{
+	char skill[] = "CBD";
+	char* skill_trees[] = {"BACDE", "CBADF", "AECB", "DBA"};
+	printf("결과값: %d \n", solution(skill, skill_trees, sizeof(skill_trees)/sizeof(skill_trees[0])));
+	//printf("사이즈는: %d \n", sizeof(skill_trees[1]));
+	return 0;
+}
