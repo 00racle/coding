7326628caa624d6948133fb1d0214bb37df7c7ba
0racle082 <haha082@hanmail.net>
2019-10-14 14:50:22 +0900

    2019.10.14 commit

---
+++ python/programmers/level2/skilltree.py
@@ -0,0 +1,22 @@
+skill = "CBD"
+skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+
+# "BCD", "CBD", "CB", "BD"
+
+def solution(skill, skill_trees):
+	answer = 0
+	for i in skill_trees:
+		s = list(skill)
+		for j in i:
+			if j not in s:
+				pass
+			elif j in s and j == s[0]:
+				s.remove(j)
+			else:
+				break
+			if j == i[-1]:
+				answer += 1
+			
+	return answer
+
+print(solution(skill, skill_trees))
