6ef43c23caf9dfde50916fed8468639f65de0160
0racle082 <haha082@hanmail.net>
2019-10-22 17:39:20 +0900

    2019.10.22 commit

--- c/programmers/level2/skilltree.c
+++ c/programmers/level2/skilltree.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//"BACDE", "CBADF", "AECB", "DBA"
 int solution(char* skill, char* skill_trees[], size_t skill_trees_len)
 {
 	int answer = 0;
@@ -10,10 +11,32 @@ int solution(char* skill, char* skill_trees[], size_t skill_trees_len)
 	{
 		//printf("%s \n", skill_trees[i]);
 		//printf("%d \n", strlen(skill_trees[i]));
-		for(j=0; j<strlen(skill_trees[i]); j++)
+		char nskill[4];
+		strcpy(nskill, skill);
+		printf("nskill 값은 : %s \n", nskill);
+		printf("skill 값은 : %s \n", skill);
+		for(; *skill_trees[i] != '\0'; skill_trees[i]++)
 		{
-			//printf("%c ", skill_trees[i][j]);
-			if(
+			printf("%c ", *(skill_trees[i]+1));
+			if(strchr(nskill, *skill_trees[i] == NULL))
+			{
+				printf("%c 패스됨 \n", *skill_trees[i]);
+				continue;
+			}
+			else if(strchr(nskill, *skill_trees[i]) && *skill_trees[i] == nskill[0])
+			{
+				strcpy(nskill, nskill+1);
+				printf("%c 해당됨 \n", *skill_trees[i]);
+			}
+			else
+			{
+				printf("%c 엘스됨 \n", *skill_trees[i]);
+				break;
+			}
+			if(*(skill_trees[i]+1) == '\0')
+			{
+				answer += 1;
+				printf("%d 여기는 0", i);
 		}
 		printf("\n");
 	}
