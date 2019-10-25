ec30ecf2abe3511fc354a795db745432482fd0c5
0racle082 <haha082@hanmail.net>
2019-10-25 18:53:04 +0900

    2019.10.25 commit

--- c/programmers/level1/zegop.c
+++ c/programmers/level1/zegop.c
@@ -2,32 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 
-// 0, 1, 4, 5, 6, 9
-// 2, 3, 7, 8
-// sqrt(
-
 long long solution(long long n)
 {
 	long long answer = 0;
-	if(sqrt(doubel(n)) == 
-	/*
-	int i;
-	int s;
-	if(n%10 == 2 || n%10 == 3 || n%10 == 7 || n%10 == 8){
+
+	if(sqrt(n) - (int)sqrt(n) == 0) 
+	{
+		answer = (sqrt(n)+1)*(sqrt(n)+1);
+	}
+	else
+	{
 		answer = -1;
 	}
-	else{
-		for(i=1; i<n/2; i++){
-			if(i*i == n){
-				s = i;
-				break;
-			}
-		}
-		answer = (s+1)*(s+1);
-	}
-	*/
-	
-
 	return answer;
 }
 
