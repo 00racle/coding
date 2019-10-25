04f7631d033b11f15dcb3ab47d668cd8f42d574f
0racle082 <haha082@hanmail.net>
2019-10-25 19:20:45 +0900

    2019.10.25 commit

---
+++ c/programmers/level1/hashad.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool solution(int x)
+{
+	bool asnwer = true;
+
+	return answer;
+}
+
+int main(void)
+{
+	int x;
+	printf("정수 입력: ");
+	scanf("%d", &x);
+	printf("결과 값은: %s \n", solution(x));
+	return 0;
+}
