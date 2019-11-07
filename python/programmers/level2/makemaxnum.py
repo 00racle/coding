02bcc9a143e565f7d121b15b0f55a5ac97776f2b
0racle082 <haha082@hanmail.net>
2019-11-08 00:47:40 +0900

    2019.11.07 commit

--- python/programmers/level2/makemaxnum.py
+++ python/programmers/level2/makemaxnum.py
@@ -1,5 +1,14 @@
 number = 1924
+#number = 1231234   = 3234
+# 1234, 1232, 1233, 1234,
+# k = 3
 k = 2
 
-def solution(number, k):
+# [19, 12, 14, 92, 94, 24]
 
+def solution(number, k):
+    answer = ''
+
+    return answer
+
+solution(number, k)
---
+++ python/programmers/level2/phonenum_list.py
@@ -0,0 +1,10 @@
+p = ["119", "97674223", "1195524421"]
+
+def solution(p):
+    answer = True
+    for i in p:
+
+        
+    return answer
+
+print(solution(p))
