c81209cc02be9a10ae1594859a8893c4edbf7840
0racle082 <haha082@hanmail.net>
2019-11-22 17:16:42 +0900

    2019.11.22 commit

--- python/dev/zippass_20191121.py
+++ python/dev/zippass_20191121.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import time
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QImage, QPalette, QBrush, QIcon, QPixmap
@@ -13,7 +15,7 @@ class MyWindow(QWidget):
         self.setGeometry(500, 200, 600, 450)
         self.setWindowTitle("Passwod Crack")
         self.setWindowIcon(QIcon('anony.png'))
-
+        '''
         #oImage = QImage("anony.png")
         oImage = QImage("anonyback.jpg")
         sImage = oImage.scaled(QSize(600, 450))
@@ -21,6 +23,7 @@ class MyWindow(QWidget):
         palette = QPalette()
         palette.setBrush(10, QBrush(sImage))
         self.setPalette(palette)
+        '''
 
         self.pushButton01 = QPushButton("파일 찾기")
         #self.pushButton01.move(20, 20)
@@ -31,20 +34,24 @@ class MyWindow(QWidget):
         self.label = QLabel("파일경로")
 
         #uplayout 에 이미지 삽입
-        #self.imglabel = QLabel()
+        self.imglabel = QLabel()
         #pixmap = QPixmap("anonyback.jpg")
-        #self.imglabel.setPixmap(pixmap)
+        pixmap = QPixmap("anonymous.jpeg")
+        self.imglabel.setPixmap(pixmap)
 
         #self.output = QTextEdit()
         self.output = QLineEdit()
+        self.lcd = QLCDNumber()
+        self.lcd.setFixedHeight(100)
 
         upLayout = QVBoxLayout()
         downLayout = QVBoxLayout()
         downLayout.addWidget(self.pushButton01)
         downLayout.addWidget(self.lineEdit)
         downLayout.addWidget(self.pushButton02)
-        downLayout.addWidget(self.output)
-        #upLayout.addWidget(self.imglabel)
+        #downLayout.addWidget(self.output)
+        downLayout.addWidget(self.lcd)
+        upLayout.addWidget(self.imglabel)
 
         layout = QVBoxLayout()
         layout.addLayout(upLayout)
@@ -59,7 +66,11 @@ class MyWindow(QWidget):
         self.lineEdit.setText(fname[0])
 
     def startCracked(self):
-        self.output.setText(self.filename)
+        for i in range(1000):
+            self.lcd.display(i)
+            self.lcd.repaint()
+            #time.sleep(0.1)
+
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
