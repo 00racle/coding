4b4692016f5cd5ac9130765317e872ab1aa061dc
0racle082 <haha082@hanmail.net>
2019-12-04 20:01:52 +0900

    2019.12.04 commit

--- c/test.c
+++ c/test.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
-struct point
+typedef struct point
 {
 	int xpos;
 	int ypos;
-};
+}Point;
 
-struct person
+void ShowPosition(Point pos)
 {
-	char name[20];
-	char phoneNum[20];
-	int age;
-};
+	printf("[%d, %d] \n", pos.xpos, pos.ypos);
+}
+
+Point GetCurrentPosition(void)
+{
+	Point cen;
+	printf("Input current pos: ");
+	scanf("%d %d", &cen.xpos, &cen.ypos);
+	return cen;
+}
 
 int main(void)
 {
-	struct point pos={10, 20};
-	struct person man={"이승기", "010-1212-0001", 21};
-
-	printf("%p %p \n", &pos, &pos.xpos);
-	printf("%p %p \n", &man, man.name);
+	Point curPos = GetCurrentPosition();
+	ShowPosition(curPos);
 	return 0;
 }
