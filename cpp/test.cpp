d224e5438c39b299c989420cb63d3e091b538ee8
0racle082 <haha082@hanmail.net>
2019-12-05 22:36:03 +0900

    2019.12.05 commit

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,62 +1,37 @@
 #include <iostream>
+
 using namespace std;
 
-#define ID_LEN		20
-#define MAX_SPD		200
-#define FUEL_STEP	2
-#define	ACC_STEP	10
-#define	BRK_STEP	10
-
-struct Car
+struct student
 {
-	char gamerID[ID_LEN];		//소유자ID
-	int fuelGauge;				//연료량
-	int curSpeed;				//현재 속도
+	private:
+		int id;
+		const char *name;
+		float percentage;
+	public:
+		void Show();
+		void SetInfo(int _id, const char *_name, float _percentage);
 };
 
-void ShowCarState(const Car &car)
+void student::Show()
 {
-	cout<<"소유자ID: "<<car.gamerID<<endl;
-	cout<<"연료량: "<<car.fuelGauge<<"%d"<<endl;
-	cout<<"현재속도: "<<car.curSpeed<<"km/s"<<endl<<endl;
-}
-void Accel(Car &car)
-{
-	if(car.fuelGauge<=0)
-		return;
-	else
-		car.fuelGauge-=FUEL_STEP;
-
-	if(car.curSpeed+ACC_STEP>=MAX_SPD)
-	{
-		car.curSpeed=MAX_SPD;
-		return;
-	}
-	car.curSpeed+=ACC_STEP;
+	cout<<"아이디: "<<id<<endl;
+	cout<<"이름: "<<name<<endl;
+	cout<<"백분률: "<<percentage<<endl;
 }
 
-void Break(Car &car)
+void student::SetInfo(int _id, const char *_name, float _percentage)
 {
-	if(car.curSpeed<BRK_STEP)
-	{
-		car.curSpeed=0;
-		return;
-	}
-	car.curSpeed-=BRK_STEP;
+	id = _id;
+	name = _name;
+	percentage = _percentage;
 }
 
 int main(void)
 {
-	Car run99={"run99", 100, 0};
-	Accel(run99);
-	Accel(run99);
-	ShowCarState(run99);
-	Break(run99);
-	ShowCarState(run99);
+	student s;
 
-	Car sped77={"Sped77", 100, 0};
-	Accel(sped77);
-	Break(sped77);
-	ShowCarState(sped77);
+	s.SetInfo(1, "김철수", 90.5);
+	s.Show();
 	return 0;
 }
