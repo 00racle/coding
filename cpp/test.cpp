0569cfc698b361b9c826b9a69b99fd7ad5dad4f0
0racle082 <haha082@hanmail.net>
2019-12-05 18:22:06 +0900

    2019.12.05 commit

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,62 +1,29 @@
 #include <iostream>
 using namespace std;
 
-#define ID_LEN		20
-#define MAX_SPD		200
-#define FUEL_STEP	2
-#define	ACC_STEP	10
-#define	BRK_STEP	10
-
-struct Car
-{
-	char gamerID[ID_LEN];		//소유자ID
-	int fuelGauge;				//연료량
-	int curSpeed;				//현재 속도
+class Point{
+	private:
+		int x;
+		int y;
+	public:
+		//디폴트 생성자
+		Point(){
+			x = 10;
+			y = 15;
+		}
+		Point(int x, int y){
+			this->x = x;
+			this->y = y;
+		}
+		void print(){
+			cout<<"X: "<<x<<", Y: "<<y<<"\n";
+		}
 };
 
-void ShowCarState(const Car &car)
-{
-	cout<<"소유자ID: "<<car.gamerID<<endl;
-	cout<<"연료량: "<<car.fuelGauge<<"%d"<<endl;
-	cout<<"현재속도: "<<car.curSpeed<<"km/s"<<endl<<endl;
-}
-void Accel(Car &car)
-{
-	if(car.fuelGauge<=0)
-		return;
-	else
-		car.fuelGauge-=FUEL_STEP;
+int main(void){
+	Point p;
+	p.print();
 
-	if(car.curSpeed+ACC_STEP>=MAX_SPD)
-	{
-		car.curSpeed=MAX_SPD;
-		return;
-	}
-	car.curSpeed+=ACC_STEP;
-}
-
-void Break(Car &car)
-{
-	if(car.curSpeed<BRK_STEP)
-	{
-		car.curSpeed=0;
-		return;
-	}
-	car.curSpeed-=BRK_STEP;
-}
-
-int main(void)
-{
-	Car run99={"run99", 100, 0};
-	Accel(run99);
-	Accel(run99);
-	ShowCarState(run99);
-	Break(run99);
-	ShowCarState(run99);
-
-	Car sped77={"Sped77", 100, 0};
-	Accel(sped77);
-	Break(sped77);
-	ShowCarState(sped77);
-	return 0;
+	Point p2 = { 3, 4 };
+	p2.print();
 }
