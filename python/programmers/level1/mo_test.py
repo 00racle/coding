679d05b10b50f90952479cd5973570d6795f04ec
0racle082 <haha082@hanmail.net>
2020-02-14 10:39:50 +0900

    2020.02.14 commit

--- python/programmers/level1/mo_test.py
+++ python/programmers/level1/mo_test.py
@@ -1,13 +1,21 @@
-#1. [1,2,3,4,5],[1,2,3,4,5]
-#2. [2,1,2,3,2,4,2,5],[2,1,2,3,2,4,2,5]
-#3. [3,3,1,1,2,2,4,4,5,5],[3,3,1,1,2,2,4,4,5,5]
-
 answers = [1,2,3,4,5]
-#answers = [1,3,2,4,2]
 
 def solution(answers):
+    s1 = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
+    s2 = []
+    ret = []
+    for i in s1:
+        s2.append(i*(len(answers)//len(i)) + i[:len(answers)%len(i)])
 
-    
+    for i in s2:
+        cnt = 0
+        for j in range(len(answers)):
+            if answers[j] == i[j]:
+                cnt += 1
 
-solution(answers)
+        ret.append(cnt)
 
+    m = max(ret)
+    return [ret.index(m)+1] if ret.count(m) == 1 else [i+1 for i in range(len(ret)) if ret[i] == m]
+
+print(solution(answers))
