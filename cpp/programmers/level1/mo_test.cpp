001ead0ccc3774283550cab4671f80e28223c789
0racle082 <haha082@hanmail.net>
2020-02-17 17:52:19 +0900

    2020.02.17 commit

---
+++ cpp/programmers/level1/mo_test.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers);
+
+int main(void)
+{
+	vector<int> answers;
+	vector<int> ret;
+	int num;
+	cout<<"answers입력: ";
+	for(int i=0; i<5; i++)
+	{
+		cin>>num;
+		answers.push_back(num);
+	}
+	
+	ret = solution(answers);
+	cout<<"결과 값은: ";
+	for(int i=0; i<ret.size(); i++)
+	{
+		cout<<ret[i]<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
+
+vector<int> solution(vector<int> answers)
+{
+	vector< vector<int> > su1 = {
+		{1,2,3,4,5},
+		{2,1,2,3,2,4,2,5},
+		{3,3,1,1,2,2,4,4,5,5}
+	};
+	int length = answers.size();
+	int cnt = 0;
+	vector< vector<int> > su2;
+	su2.reserve(3);
+	vector<int> answer;
+	for(int i=0; i<3; i++)
+	{
+		int m = length/su1[i].size();
+		int n = length%su1[i].size();
+		for(int j=0; j<m; j++)
+		{
+			su2[i].insert(su2[i].end(), su1[i].begin(), su1[i].end());
+		}
+		su2[i].insert(su2[i].end(), su1[i].begin(), su1[i].begin() + n);
+	}
+	for(int i=0; i<3; i++)
+	{
+		
+	}
+	return answer;
+}
